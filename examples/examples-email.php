5f10e79cd8a2f0644c06e10e4f5e525552d91e8c
cs-eliseev <a.k.eliseev@gmail.com>
2019-03-01 12:16:31 +0300

    add example exist email

--- examples/examples-email.php
+++ examples/examples-email.php
@@ -28,3 +28,12 @@ var_dump(Email::is('mail@google.com'));
 // false
 var_dump(Email::is('Example text mail@google.com'));
 echo PHP_EOL;
+
+// Example: exist email
+// true
+var_dump(Email::exist('mail@google.com'));
+// true
+var_dump(Email::exist('Example text mail@google.com'));
+// false
+var_dump(Email::exist('Example text mail@inbox.com', '([a-z]+@google.com)'));
+echo PHP_EOL;
