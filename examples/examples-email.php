9695a1143d77e059511b79e9f64ec2e8f5c698d1
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-06 07:54:30 +0300

    add label info

--- examples/examples-email.php
+++ examples/examples-email.php
@@ -5,33 +5,37 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autol
 use cse\helpers\Email;
 
 // Example: hide short mail
+$label = 'Hide short mail: ';
 // m***@google.com
-var_dump(Email::hide('mail@google.com'));
+var_dump($label . Email::hide('mail@google.com'));
 // Example: hide medium mail
 // e***l@google.com
-var_dump(Email::hide('email@google.com'));
+var_dump($label . Email::hide('email@google.com'));
 // Example: hide large mail
 // e***l***8@google.com
-var_dump(Email::hide('email2018@google.com'));
+var_dump($label . Email::hide('email2018@google.com'));
 echo PHP_EOL;
 
 // Example: check domain to email
+$label = 'Check domain to email: ';
 // true
-var_dump(Email::checkDomain('mail@google.com', 'google.com'));
+var_dump($label . Email::checkDomain('mail@google.com', 'google.com'));
 // false
-var_dump(Email::checkDomain('mail@mail.ru', 'google.com'));
+var_dump($label . Email::checkDomain('mail@mail.ru', 'google.com'));
 echo PHP_EOL;
 
 // Example: check email
+$label = 'Check email: ';
 // true
-var_dump(Email::is('mail@google.com'));
+var_dump($label . Email::is('mail@google.com'));
 // false
-var_dump(Email::is('Example text mail@google.com'));
+var_dump($label . Email::is('Example text mail@google.com'));
 echo PHP_EOL;
 
 // Example: extract email
+$label = 'Extract email: ';
 //  mail@google.com
-var_dump(Email::extract('Example text mail@google.com'));
+var_dump($label . Email::extract('Example text mail@google.com'));
 // null
-var_dump(Email::extract('Example text mail@inbox.com', '([a-z]+@google.com)'));
+var_dump($label . Email::extract('Example text mail@inbox.com', '([a-z]+@google.com)'));
 echo PHP_EOL;
